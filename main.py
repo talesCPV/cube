76665c23da391757eba8658ec5c7a5c77e538c63
talesCPV <tales@flexibus.com.br>
2020-02-28 11:38:18 -0300

    28/02/20

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-"""
- Simulation of a rotating 3D Cube
- Developed by Leonel Machava <leonelmachava@gmail.com>
 
-http://codeNtronix.com
-
-"""
 import sys, math, pygame
-from operator import itemgetter
+
 
 pygame.init()
 
@@ -16,12 +10,23 @@ pygame.display.set_caption("Teste de Cubo")
 clock = pygame.time.Clock()
 
 angle = [0,0,0]
-
-center = [300,300]
+raio = 10
+pivot = [300,300]
+pointlist = [(10, 10), (10, 20),
+             (10, 20), (20, 20),
+             (20, 20), (20, 30),
+             (20, 30), (10, 10)]
 
 def setPoint():
     for i in range(8):
-        print(i)
+        pointlist[i] = (pivot[0],getX(90))
+
+
+def getX(angle):
+    saida =  raio * math.sin(math.radians(angle))
+    saida -= pivot[0]
+    return saida
+
 
 while 1:
 
@@ -30,16 +35,14 @@ while 1:
             pygame.quit()
             sys.exit()
 
-            self.clock.tick(60)
-            self.screen.fill((0, 0, 0)) #background
+#            self.clock.tick(60)
+            screen.fill((0, 0, 0)) #background
 
-    pointlist = [(10,10), (10,20),
-                 (10,20), (20,20),
-                 (20,20), (20,30),
-                 (20,30), (10,10)]
-
-    pygame.draw.line(screen, (255, 255, 255), center, pointlist[0] )
+    pygame.draw.line(screen, (255, 255, 255), pivot, pointlist[0] )
 #    pygame.draw.polygon(screen, (255,200,150), pointlist)
 
 
     pygame.display.flip()
+
+    print(getX(90))
+    setPoint()
